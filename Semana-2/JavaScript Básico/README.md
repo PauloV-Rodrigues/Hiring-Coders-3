29d6d559a00554671173a522e67c7243f668dd46
PauloV-Rodrigues <ads.rodriguespaulo@gmail.com>
2022-05-12 23:08:57 -0300

    Updated em 12-05-22

---
+++ Semana-2/JavaScript Básico/README.md
@@ -0,0 +1,11 @@
+# JavaScript Básico
+## Aplicação: Validador de CPF
+
+### Homepage
+![Home](screenshot.png)
+
+### CPF Inválido
+![Invalido](screenshot.png)
+
+### CPF Válido
+![Valido](screenshot.png)
--- Semana-2/JavaScript Básico/script.js
+++ Semana-2/JavaScript Básico/script.js
@@ -7,26 +7,24 @@ function validaCPF(cpf) {
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
 
-        var soma = 0;
-
         // Validação do Primeiro Dígito
+        var soma = 0;
         for(var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
         }
         var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
-        if (resultado != digitos.chatAt(0)) {
+        if (resultado != digitos.charAt(0)) {
             return false;
         }
 
+        // Validação do Segundo Dígito
         soma = 0;
         numeros = cpf.substring(0, 10);
-        
-        // Validação do Segundo Dígito
         for(var k = 11; k > 1; k--){
             soma += numeros.charAt(11 - k) * k;
         }
         resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
-        if (resultado != digitos.chatAt(1)) {
+        if (resultado != digitos.charAt(1)) {
             return false;
         }
 
