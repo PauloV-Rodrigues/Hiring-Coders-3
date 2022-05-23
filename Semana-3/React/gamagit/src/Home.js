523e31cccfb8f1f02baeda3fac61fff06e215704
PauloV-Rodrigues <ads.rodriguespaulo@gmail.com>
2022-05-23 19:20:01 -0300

    Updated em 23-05-22

--- Semana-3/React/gamagit/src/Home.js
+++ Semana-3/React/gamagit/src/Home.js
@@ -2,18 +2,27 @@ import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import * as $ from './styled'
 
 function App(props) {
   const [ usuario, setUsuario] = useState('');
+  
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+    });
   }
 
   return (
-    <>
-      <input className="usuarioInput" placeholder="Usuario" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+    <$.Container>
+      <$.Input className="usuarioInput" placeholder="Usuario" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <$.Button type="button" onClick={handlePesquisa}>Pesquisar</$.Button>
+    </$.Container>
   );
 }
 
